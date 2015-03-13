c97f948a9b68654309dcd358aa2bd432d76235bb
Alex <Lex720@bitbucket.org>
2015-03-13 14:01:38 -0430

    Economica font

--- app/views/index.blade.php
+++ app/views/index.blade.php
@@ -145,26 +145,26 @@
                         <div class="da-slide">
                             <h2>Easy management</h2>
                             <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts. Separated they live in Bookmarksgrove right at the coast of the Semantics, a large language ocean.</p>
-                            <a href="#" class="da-link">Read more</a>
-                            <!--<div class="da-img"> {{ HTML::image('assets/img/slider/parallax/1.png') }} </div>-->
+                            <!--<a href="#" class="da-link">Read more</a>
+                            <div class="da-img"> {{ HTML::image('assets/img/slider/parallax/1.png') }} </div>-->
                         </div>
                         <div class="da-slide">
                             <h2>Revolution</h2>
                             <p>A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth.</p>
-                            <a href="#" class="da-link">Read more</a>
-                            <!--<div class="da-img"> {{ HTML::image('assets/img/slider/parallax/2.png') }} </div>-->
+                            <!--<a href="#" class="da-link">Read more</a>
+                            <div class="da-img"> {{ HTML::image('assets/img/slider/parallax/2.png') }} </div>-->
                         </div>
                         <div class="da-slide">
                             <h2>Warm welcome</h2>
                             <p>When she reached the first hills of the Italic Mountains, she had a last view back on the skyline of her hometown Bookmarksgrove, the headline of Alphabet Village and the subline of her own road, the Line Lane.</p>
-                            <a href="#" class="da-link">Read more</a>
-                            <!--<div class="da-img"> {{ HTML::image('assets/img/slider/parallax/3.png') }} </div>-->
+                            <!--<a href="#" class="da-link">Read more</a>
+                            <div class="da-img"> {{ HTML::image('assets/img/slider/parallax/3.png') }} </div>-->
                         </div>
                         <div class="da-slide">
                             <h2>Quality Control</h2>
                             <p>Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Grammar.</p>
-                            <a href="#" class="da-link">Read more</a>
-                            <!--<div class="da-img"> {{ HTML::image('assets/img/slider/parallax/4.png') }} </div>-->
+                            <!--<a href="#" class="da-link">Read more</a>
+                            <div class="da-img"> {{ HTML::image('assets/img/slider/parallax/4.png') }} </div>-->
                         </div>
                         <nav class="da-arrows">
                             <span class="da-arrows-prev"></span>
@@ -627,7 +627,7 @@
             <div class="container-fluid">
                 <div class="row">
                 
-                    COPYRIGHT © 2014 Visionary Projects, C.A. Todo los derechos reservados.    
+                    <p>COPYRIGHT © 2014 Visionary Projects, C.A. Todo los derechos reservados.</p>
                 
                 </div>
             </div>  
